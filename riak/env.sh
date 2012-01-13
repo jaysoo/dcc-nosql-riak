f77334ee82c0d99e28a59e7173ce5377eaf1f5a1
ubuntu <ubuntu@dcc-1.oicr.on.ca>
2012-01-13 17:19:47 -0500

    init

---
+++ riak/env.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export RIAK_HOST=10.0.3.66
+export RIAK_PORT=8098
---
+++ riak/load_data.js
@@ -0,0 +1,103 @@
+/*
+ * Parses a TSV file and loads the data into Riak. The first line of the TSV file must be the header.
+ *
+ * You also need to set the Riak host and port as RIAK_HOST and RIAK_PORT environment variables respectively.
+ *
+ * Usage:
+ *   node load_data.js [file]
+ *
+ */
+
+// Imports
+var riak = require('riak-js').getClient({ 
+        host: process.env['RIAK_HOST'], 
+        port: process.env['RIAK_PORT'] 
+    }),
+    _ = require('underscore'),
+    csv = require('csv'),
+    fs = require('fs'),
+    // seq = require('seq'),
+    num_cpus = require('os').cpus().length;
+
+if (process.argv.length < 3)
+    console.log('\033[0;31m' + process.argv[1] + ' requires a file argument\033[0m'), process.exit(1);
+
+// Helper vars and functions
+var count = 0 /* counter for total rows loaded */,
+
+    total_lines_est = 100000,
+
+    file = process.argv[2],
+
+    // Use the file name (minus extension) as Riak bucket name
+    bucket = file.split('.')[0].split('/').pop(),
+
+    // array of headers parsed
+    headers = null,
+
+    // For timing total run time
+    timer = { start: new Date().getTime(), end: null };
+
+console.log('\033[0;33mLoading data into "' + bucket + '" bucket...\n\033[0m');
+
+
+// Might be using relative path
+if ( !(/^\//).test(file) )
+    file = __dirname + '/' + file;
+
+function clean(data) {
+    return _.map(data, function(value) {
+        return value == '\\N'
+            ? null
+            : value;
+    });
+}
+
+function begin() {
+    // Open TSV file and load all contents to Riak
+    csv()
+        .fromPath(file, {
+            delimiter: '\t'
+        })
+
+        .on('data', function(data, index) {
+
+            data =  clean(data);
+
+            if (count++ == 0) {
+                headers = data;
+            } else {
+                var json_data = {};
+
+                // populate keys and values
+                for (var i = 0; i < data.length; i++)
+                    json_data[headers[i]] = data[i];
+
+                riak.save(bucket, data[0], json_data, {}, function(error) {
+                    if (error != null)
+                        console.log(error);
+                });
+            }
+        })
+
+        .on('end', function() {
+
+            console.log('\033[0;33m\n...Loaded ' + count + ' rows\033[0m');
+
+            timer.end = new Date().getTime();
+
+            console.log('\033[0;34m\nStats:');
+
+            var total_time = ( timer.end - timer.start ) / 1000;
+            
+            console.log(
+                '    Total time = ' + total_time.toFixed(2) + ' seconds\n' +
+                '    Inserts = ' + count + '\n' +
+                '    Operations per second = ' + ( count / total_time ).toFixed(2) + '\n\033[0m'
+            );
+        });
+}
+
+begin();
+
+
