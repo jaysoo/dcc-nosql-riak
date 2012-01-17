b4552c925764b01263b84d8ed26a25ddb5872675
Jack Hsu <jack.hsu@gmail.com>
2012-01-17 12:13:38 -0500

    Add range support for 2i queries

--- riak/python/search.py
+++ riak/python/search.py
@@ -3,13 +3,17 @@ import sys, pprint, time
 import riak
 from settings import settings
 
-def query(bucket_name, field, value):
+def query(bucket_name, field, value, end_value=None):
     client = riak.RiakClient(host=settings['HOST'], port=settings['PORT'], transport_class=settings['TRANSPORT_CLASS'])
     pp = pprint.PrettyPrinter(depth=2, indent=4)
 
     start_time = time.time()
 
-    results = client.index(bucket_name, field, value).run()
+    if end_value:
+        print 'using range values'
+        results = client.index(bucket_name, field, value, end_value).run()
+    else:
+        results = client.index(bucket_name, field, value, end_value).run()
 
     end_time = time.time()
     elapsed_time  = end_time - start_time
@@ -30,6 +34,8 @@ if __name__ == '__main__':
         print '\033[0;31mthree arguments required\033[0m'
         sys.exit(1)
 
-    query(sys.argv[1], sys.argv[2] + '_bin', sys.argv[3])
-
+    try:
+        query(sys.argv[1], sys.argv[2] + '_bin', sys.argv[3], sys.argv[4])
+    except IndexError:
+        query(sys.argv[1], sys.argv[2] + '_bin', sys.argv[3])
 
