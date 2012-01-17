a697440642751278264810c0e53dc212dad4ac3a
Jack Hsu <jack.hsu@gmail.com>
2012-01-17 12:05:34 -0500

    Display query time in search.py

--- riak/python/search.py
+++ riak/python/search.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
-import sys, pprint
+import sys, pprint, time
 import riak
 from settings import settings
 
 def query(bucket_name, field, value):
     client = riak.RiakClient(host=settings['HOST'], port=settings['PORT'], transport_class=settings['TRANSPORT_CLASS'])
-    results = client.index(bucket_name, field, value).run()
-    pp = pprint.PrettyPrinter(depth=6)
+    pp = pprint.PrettyPrinter(depth=2, indent=4)
 
-    for link in results:
+    start_time = time.time()
+
+    results = client.index(bucket_name, field, value).run()
+
+    end_time = time.time()
+    elapsed_time  = end_time - start_time
+
+    print '\033[0;33mFound %s document(s)...\n\033[0m' % len(results)
+    print '\033[0;33mDisplaying top 5 results...\n\033[0m'
+
+    for i,link in enumerate( results[:5] ):
+        print '#%s:' % (i + 1)
         pp.pprint( link.get().get_data() )
+        print 
+
+    print '\033[0;34mQuery time: %0.2f seconds\n\033[0m' % elapsed_time
+
 
 if __name__ == '__main__':
     if len( sys.argv ) < 4:
